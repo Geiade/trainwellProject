1fc3cecf5d08b2713c13ebca96e5ce80aa3dad0d
bilaluali <bilaluali98@gmail.com>
2020-03-29 17:12:46 +0200

    Included trainWellApp urls. Added meta tag on base.html for responsive website.

--- trainwell/urls.py
+++ trainwell/urls.py
@@ -20,10 +20,11 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='trainwell:index'), name='index'),
     path('admin/', admin.site.urls),
-    # path('trainwell/', include('trainWellApp.urls', 'trainWellApp')),
+
     path('accounts/login/', auth_views.LoginView.as_view(), name="login"),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('trainwell/', include('trainWellApp.urls')),
-    path('', RedirectView.as_view(url=''))
+    path('trainwell/', include(('trainWellApp.urls', 'trainWellApp'), namespace='trainwell')),
+
 ]
