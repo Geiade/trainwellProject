1be5039da268f9f257913483c952a20559a7da64
bilaluali <bilaluali98@gmail.com>
2020-05-01 19:00:44 +0200

    Defined queryset to get active incidences and done incidences.

--- trainWellApp/views/management.py
+++ trainWellApp/views/management.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.views.generic import ListView
 
 from trainWellApp.forms import EventForm
-from trainWellApp.models import Selection
+from trainWellApp.models import Selection, Incidence
 
 
 def addEvent(request):
@@ -31,3 +32,24 @@ def _get_affected_bookings(init, end):
             bookinglist[s.booking] = [s]
 
     return bookinglist
+
+
+class IncidencesListView(ListView):
+
+    model = Incidence
+    template_name = 'trainWellApp/incidences_list.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'done': self.get_queryset1()})
+        return context
+
+
+    def get_queryset(self):
+        return self.model.objects.filter(done=False).order_by('limit_date')
+
+
+    def get_queryset1(self):
+        return self.model.objects.filter(done=True).order_by('limit_date')
+
